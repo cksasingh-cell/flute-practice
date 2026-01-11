d735116d76e538ce611932fe1c429c3c998c681b
cksasingh-cell <cksasingh@gmail.com>
2026-01-11 19:45:53 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+    CLIENT_ID: '744375413516-216ck8kv3r786p4s0fre7ujkfrnm6ji8.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyAiFYIbHSCLBVV6wJv5jT0T2a9FGmF_tMY',
+    FOLDER_NAME: 'Flute Practice Surs'
+};
